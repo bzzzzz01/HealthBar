63f2b910d506209a87436749c67e677d6f15702b
bzzzzz01 <905161357@qq.com>
2018-06-10 01:07:48 +0800

    Add files via upload

---
+++ HealthBarIMGUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class HealthBarIMGUI : MonoBehaviour
+{
+    float HP = 0.5f;
+    Rect HealthBar;
+    Rect minusHP, plusHP;
+
+    void Start()
+    {
+        HealthBar = new Rect(Screen.width / 2 - 50, Screen.height / 2 - 20, 100, 10);
+        minusHP = new Rect(Screen.width / 2 - 50, Screen.height / 2,  50, 20);
+        plusHP = new Rect(Screen.width / 2, Screen.height / 2, 50, 20);
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(minusHP, "-HP"))
+        {
+            if (HP > 0.1f)
+            {
+                HP -= 0.1f;
+            }
+        }
+        if (GUI.Button(plusHP, "+HP"))
+        {
+            if (HP < 1f)
+            {
+                HP += 0.1f; 
+            }
+        }
+        GUI.HorizontalScrollbar(HealthBar, 0, HP, 0, 1);
+    }
+}
---
+++ HealthBarUGUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarUGUI : MonoBehaviour
+{
+
+    public RectTransform HPbar;
+    
+    void Update()
+    {
+        this.gameObject.transform.Translate(Input.GetAxis("Horizontal") * 10 * Time.deltaTime, 0, Input.GetAxis("Vertical") * 10 * Time.deltaTime);
+        
+        Vector2 vec2 = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
+        HPbar.anchoredPosition = new Vector2(vec2.x - Screen.width / 2 , vec2.y - Screen.height / 2 + 50);
+    }
+
+}
